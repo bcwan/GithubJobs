ce7f0521a8ba75e3a88f2e076a9eb7300ca1f300
bcwan <bcwan8@gmail.com>
2021-06-06 16:19:43 -0700

    Add job component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import useFetchJobs from './useFetchJobs';
 import { Container } from 'react-bootstrap';
+import React, { useState } from 'react';
 
 function App() {
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
   const { jobs, loading, error } = useFetchJobs();
 
   return (
     <Container>
       {loading && <h1>Loading...</h1>}
       {error && <h1>Error. Try refreshing.</h1>}
-      <h1>{jobs.length}</h1>
+      {jobs.map(job => {
+        return <Job key={job.id} job={job} />
+      })}
     </Container>
   );
 }
