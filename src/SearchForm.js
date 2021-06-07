aa37b99038ed106826cf1635270b249e578ccd04
bcwan <bcwan8@gmail.com>
2021-06-06 21:47:00 -0700

    Add form to filter job contents

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { Form } from 'react-bootstrap';
+import { Form, Col } from 'react-bootstrap';
 
 export default function SearchForm({ params, onParamChange }) {
   return (
     <Form className="mb-4">
-      <Form.Row>
+      <Form.Row className="align-items-end">
         <Form.Group as={Col}>
           <Form.Label>Description</Form.Label>
           <Form.Control onChange={onParamChange} value={params.description} name='description' type="text"/>
+        </Form.Group>
+        <Form.Group as={Col}>
+          <Form.Label>Location</Form.Label>
+          <Form.Control onChange={onParamChange} value={params.location} name='location' type="text"/>
+        </Form.Group>
+        <Form.Group as={Col} xs="auto" className="ml-2">
+          <Form.Check onChange={onParamChange} value={params.full_time} className="mb-2" name='full_time' id="full_time" label="Only Full Time" type="checkbox"/>
         </Form.Group>
       </Form.Row>
     </Form>
